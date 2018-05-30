8b4f27a3dbefcc1a757b5e0ee2a70063ee19a32f
kimakuh <kimakuh@gmail.com>
2018-05-30 13:40:37 +0900

    .

--- bitcamp-java-project/src45/main/java/bitcamp/java106/pms/servlet/board/BoardDeleteServlet.java
+++ bitcamp-java-project/src45/main/java/bitcamp/java106/pms/servlet/board/BoardDeleteServlet.java
@@ -1,7 +1,6 @@
 package bitcamp.java106.pms.servlet.board;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,12 +11,11 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.context.ApplicationContext;
 
 import bitcamp.java106.pms.dao.BoardDao;
-import bitcamp.java106.pms.domain.Board;
 import bitcamp.java106.pms.support.WebApplicationContextUtils;
 
 @SuppressWarnings("serial")
-@WebServlet("/board/list")
-public class BoardListServlet extends HttpServlet {
+@WebServlet("/board/delete")
+public class BoardDeleteServlet extends HttpServlet {
     
     BoardDao boardDao;
     
@@ -28,36 +26,33 @@ public class BoardListServlet extends HttpServlet {
                         this.getServletContext()); 
         boardDao = iocContainer.getBean(BoardDao.class);
     }
-    
+
     @Override
     protected void doGet(
             HttpServletRequest request, 
             HttpServletResponse response) throws ServletException, IOException {
         
         try {
-            // JSP에서 출력할 게시물 목록을 가져온다.
-            List<Board> list = boardDao.selectList();
-            
-            // JSP가 게시물 목록을 사용할 수 있도록 ServletRequest 보관소에 저장한다.
-            request.setAttribute("list", list);
-            
-            // include 한다면, 이 서블릿에서 콘텐트 타입을 지정해야 한다.
-            response.setContentType("text/html;charset=UTF-8");
-            
-            // JSP를 실행한다. 실행 완료 후 이 서블릿으로 되돌아 온다.
-            request.getRequestDispatcher("/board/list.jsp").include(request, response);
+            int no = Integer.parseInt(request.getParameter("no"));
+            int count = boardDao.delete(no);
+            if (count == 0) {
+                throw new Exception("해당 게시물이 없습니다.");
+            }
+            request.setAttribute("viewUrl", "redirect:list.do");
             
         } catch (Exception e) {
-            request.setAttribute("error", e);
-            request.setAttribute("title", "게시물 목록조회 실패!");
-            request.getRequestDispatcher("/error.jsp").forward(request, response);
+            throw new ServletException(e); 
         }
     }
+    
 }
 
+//ver 45 - 프론트 컨트롤러 적용
 //ver 42 - JSP 적용
 //ver 39 - forward 적용
-//ver 37 - BoardListController를 서블릿으로 변경
+//ver 38 - redirect 적용
+//ver 37 - BoardDeleteController를 서블릿으로 변경
+//         결과를 HTML로 출력
 //ver 31 - JDBC API가 적용된 DAO 사용
 //ver 28 - 네트워크 버전으로 변경
-//ver 26 - BoardController에서 list() 메서드를 추출하여 클래스로 정의. 
+//ver 26 - BoardController에서 delete() 메서드를 추출하여 클래스로 정의. 
