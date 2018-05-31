9f22a6d3cce23ee77a96ca5423337db01a84c2e5
kimakuh <kimakuh@gmail.com>
2018-05-31 10:50:58 +0900

    .

--- bitcamp-spring-ioc/src/main/java/bitcamp/java106/step11/AppConfig01.java
+++ bitcamp-spring-ioc/src/main/java/bitcamp/java106/step11/AppConfig01.java
@@ -4,9 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
-@ComponentScan(basePackages = "bitcamp.java106.step11", excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "bitcamp.java106.step11.b.*"))
+@ComponentScan(
+    basePackages="bitcamp.java106.step11",
+    excludeFilters=@ComponentScan.Filter(
+        type=FilterType.REGEX, 
+        pattern="bitcamp.java106.step11.b.*")
+)
 public class AppConfig01 {
-    @Bean
+    @Bean 
     public Car car2() {
         Car c = new Car(null);
         c.setMaker("비트자동차");
@@ -16,3 +21,12 @@ public class AppConfig01 {
         return c;
     }
 }
+
+
+
+
+
+
+
+
+
