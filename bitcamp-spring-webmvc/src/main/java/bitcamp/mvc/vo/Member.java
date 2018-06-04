7203e492f45044e8bac27630e79e587963f58ca2
kimakuh <kimakuh@gmail.com>
2018-06-04 16:45:02 +0900

    .

--- bitcamp-spring-webmvc/src/main/java/bitcamp/mvc/vo/Member.java
+++ bitcamp-spring-webmvc/src/main/java/bitcamp/mvc/vo/Member.java
@@ -5,36 +5,29 @@ public class Member {
     String email;
     String password;
     
-    
     @Override
     public String toString() {
         return "Member [id=" + id + ", email=" + email + ", password=" + password + "]";
     }
-
+    
     public String getId() {
         return id;
     }
-
     public void setId(String id) {
         this.id = id;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
     
     
-
 }
