2f48545e2a85839343efb675819fdcec28e403f2
kimakuh <kimakuh@gmail.com>
2018-06-05 11:30:24 +0900

    .

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/web/advice/GlobalBindingInitializer.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/web/advice/GlobalBindingInitializer.java
@@ -1,23 +1,25 @@
 package bitcamp.java106.pms.web.advice;
 
 import java.beans.PropertyEditorSupport;
-import java.sql.Date;
 
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.InitBinder;
 
-@ControllerAdvice
+import bitcamp.java106.pms.domain.Classroom;
+import bitcamp.java106.pms.domain.Task;
+import bitcamp.java106.pms.web.TeamController;
+
+@ControllerAdvice(assignableTypes = { TeamController.class, Classroom.class, Task.class })
 public class GlobalBindingInitializer {
     // 컨트롤러 어드바이스 @InitBinder 메서드를 등록하면
-    // 각 페이지 컨트롤러마다 따로 등록할 필요가 없다.
-
+    // 각 페이지 컨트롤러 마다 따로 등록할 필요가 없다.
     @InitBinder
     public void initBinder(WebDataBinder binder) {
         binder.registerCustomEditor(java.sql.Date.class, new PropertyEditorSupport() {
             @Override
             public void setAsText(String text) throws IllegalArgumentException {
-                this.setValue(Date.valueOf(text));
+                this.setValue(java.sql.Date.valueOf(text));
             }
         });
     }
