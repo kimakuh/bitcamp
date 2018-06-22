8b400904bde867325d19855448feb2cd36040c0c
kimakuh <kimakuh@gmail.com>
2018-06-22 17:30:28 +0900

    '.'

--- bitcamp-spring-webmvc/src/main/java/bitcamp/mvc/web/Exam10_2.java
+++ bitcamp-spring-webmvc/src/main/java/bitcamp/mvc/web/Exam10_2.java
@@ -1,12 +1,14 @@
-// JSON 데이터 출력하기 - 적용 전 
+// JSON 데이터 출력하기 - Gson 라이브러리로 JSON 데이터 출력하기 
 package bitcamp.mvc.web;
 
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
 
 import bitcamp.mvc.dao.BoardDao;
 import bitcamp.mvc.vo.Board;
@@ -21,14 +23,16 @@ public class Exam10_2 {
         this.boardDao = boardDao;
     }
     
-    @GetMapping("list")
-    public String list(Model model) {
+    @GetMapping(value="list", produces="text/plain;charset=UTF-8")
+    @ResponseBody
+    public String list() {
         List<Board> boards = boardDao.selectList();
-        model.addAttribute("boards", boards);
         
-        // JSP로 보내서 DAO가 리턴한 게시물 목록을 가지고 HTML을 만들게 한다.
-        // 그렇게 만든 HTML은 웹브라우저로 보내져서 출력될 것이다.
-        return "exam10_1";
+        // Gson 라이브러리를 이용하여 객체를 JSON 문자열로 변환한다.
+        String json = new Gson().toJson(boards);
+        
+        // 그리고 그 JSON 문자열을 출력한다.
+        return json;
     }
 }
 
